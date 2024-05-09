05cd2b63137c9b61f06c0962d78d81dd739fe26c
sashapoliti <sas.politi@gmail.com>
2024-05-09 22:21:28 +0200

    hero fix

--- Views/header.php
+++ Views/header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,11 +21,12 @@
     <script src="https://unpkg.com/axios@1.6.7/dist/axios.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js" type="text/javascript" defer></script>
-    <title>PHP-OOP-2</title>
+    <title>Zoolean</title>
 
 </head>
 
 <body>
+    
     <nav class="class3Navbar">
         <ul class="d-flex">
             <a href="#">
@@ -49,5 +55,3 @@
         </ul>
 
     </nav>
-
-    
--- index.php
+++ index.php
@@ -8,19 +8,23 @@ $foods = Prod::create('/foods_db.json', 'Foods');
 $games = Prod::create('/games_db.json', 'Games');
 $accessories = Prod::create('/accessory_db.json', 'Accessories');
 ?>
-<section>
-    <div class="class3Hero">
-        <div class="class3Layer">
-            <div class="class3HeroText">
-                <img class="class3Logo mb-3" src="images/PetLogo.jpg" alt="logo">
-                
-                <h2 class="mb-3">Cibo - Giochi - Accessori</h2>
-                <h1 class="fw-bold fs-1"> Scopri, Scegli, Sorridi: il tuo e-shop per ogni animale!</h1>
-            </div>
+
+<div class="class3Hero  d-flex align-items-center justify-content-center">
+    <div class="content d-flex flex-column align-items-center justify-content-center">
+        <div class="logo d-flex align-items-center justify-content-center ">
+            <img src="https://images.vexels.com/media/users/3/237180/isolated/preview/888bef446c00874a98cd7614630a3d48-simple-fish-in-fish-tank-doodle.png"
+                alt="Immagine logo">
+            <h1>Zoolean</h1>
         </div>
-        <video autoplay muted loop src="images/853913-hd_1920_1080_25fps.mp4" style="width: 100%;" type="video/mp4 "></video>
+        <h2> Scopri, Scegli, Sorridi: il tuo e-shop per ogni animale!</h2>
+        <h6>Scopri il nostro mondo a misura di acquari e animali</h6>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Ricerca un prodotto" aria-label="Search">
+        </form>
     </div>
-</section>
+    <video autoplay muted loop src="images/853913-hd_1920_1080_25fps.mp4" type="video/mp4 "></video>
+</div>
+
 
 <main id="app" class="class3Main">
 
@@ -37,15 +41,16 @@ $accessories = Prod::create('/accessory_db.json', 'Accessories');
                             <p class="card-text fw-bolder"><?= $food->description ?></p>
                             <p class="card-text fs-3"><?= $food->category->name ?></p>
                             <p class="card-text fs-2"><i class="<?= $food->category->icon ?>"></i></p>
-                            <button @click="addToCart(<?= $food->id ?>, '/foods_db.json')" class="btn btn-primary class3Button">
-                            Aggiungi</button>
+                            <button @click="addToCart(<?= $food->id ?>, '/foods_db.json')"
+                                class="btn btn-primary class3Button">
+                                Aggiungi</button>
                         </div>
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
     </section>
-    
+
     <!-- toys section -->
     <section class="container">
         <h2 class="text-center fs-1 mb-5">Giochi per animali</h2>
@@ -59,7 +64,8 @@ $accessories = Prod::create('/accessory_db.json', 'Accessories');
                             <p class="card-text fw-bolder"><?= $game->description ?></p>
                             <p class="card-text fs-3"><?= $game->category->name ?></p>
                             <p class="card-text fs-2"><i class="<?= $game->category->icon ?>"></i></p>
-                            <button @click="addToCart(<?= $game->id ?>, '/games_db.json')" class="btn btn-primary class3Button">Aggiungi</button>
+                            <button @click="addToCart(<?= $game->id ?>, '/games_db.json')"
+                                class="btn btn-primary class3Button">Aggiungi</button>
                         </div>
                     </div>
                 </div>
@@ -74,22 +80,24 @@ $accessories = Prod::create('/accessory_db.json', 'Accessories');
             <?php foreach ($accessories as $accessory): ?>
                 <div class="col-4">
                     <div class="card class3Card mb-3">
-                        <img class="class3CardImg" src="<?= $accessory->image ?>" class="class3CardImg" alt="<?= $accessory->name ?>">
+                        <img class="class3CardImg" src="<?= $accessory->image ?>" class="class3CardImg"
+                            alt="<?= $accessory->name ?>">
                         <div class="class3CardBody">
                             <h5 class="card-title fs-3"><?= $accessory->name ?></h5>
                             <p class="card-text fw-bolder"><?= $accessory->description ?></p>
                             <p class="card-text fs-3"><?= $accessory->category->name ?></p>
                             <p class="card-text fs-2"><i class="<?= $accessory->category->icon ?>"></i></p>
-                            <button @click="addToCart(<?= $accessory->id ?>, '/accessory_db.json')" class="btn btn-primary class3Button">Aggiungi</button>
+                            <button @click="addToCart(<?= $accessory->id ?>, '/accessory_db.json')"
+                                class="btn btn-primary class3Button">Aggiungi</button>
                         </div>
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
     </section>
-   
+
 </main>
 
 <?php
 include __DIR__ . "/Views/footer.php"
-?>
+    ?>
